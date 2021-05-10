16898d99e5f32fe8373819e37c52842c2914470d
lBecker <lbecker@gravitationalmarketing.com>
2021-05-10 11:12:32 -0400

    Implement Placeholder dropdown plugin

--- packages/ckeditor5-build-markdown/src/ckeditor.js
+++ packages/ckeditor5-build-markdown/src/ckeditor.js
@@ -30,6 +30,7 @@ import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefrom
 import Table from '@ckeditor/ckeditor5-table/src/table';
 import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar';
 import TextTransformation from '@ckeditor/ckeditor5-typing/src/texttransformation';
+import PlaceholderDropDown from './placeholderDropDown';
 
 export default class MarkdownEditor extends ClassicEditorBase {}
 
@@ -58,7 +59,8 @@ MarkdownEditor.builtinPlugins = [
 	PasteFromOffice,
 	Table,
 	TableToolbar,
-	TextTransformation
+	TextTransformation,
+	PlaceholderDropDown
 ];
 
 // Editor configuration.
@@ -81,7 +83,8 @@ MarkdownEditor.defaultConfig = {
 			'insertTable',
 			'mediaEmbed',
 			'undo',
-			'redo'
+			'redo',
+			'placeholderdropdown'
 		]
 	},
 	image: {
@@ -99,6 +102,21 @@ MarkdownEditor.defaultConfig = {
 			'mergeTableCells'
 		]
 	},
+	dropdownlist: [
+		{ label: 'Account Name', value: '{{account.name}}' },
+		{ label: 'Sender Email', value: '{{template.senderEmailAddress}}' },
+		{ label: 'Sender Name', value: '{{template.senderName}}' },
+		{ label: 'Lead\'s First Name', value: '{{lead.contact.name.first}}' },
+		{ label: 'Lead\'s Last Name', value: '{{lead.contact.name.last}}' },
+		{ label: 'Lead\'s Full Name', value: '{{lead.contact.name.full}}}}' },
+		{ label: 'Lead\'s Email', value: '{{lead.contact.email.address}}' },
+		{ label: 'Lead\'s Phone Number', value: '{{lead.contact.phone.full}}' },
+		{ label: 'Appointment Date', value: '{{lead.appointment.scheduledDate}}' },
+		{ label: 'Store Name', value: '{{lead.store.name}}' },
+		{ label: 'User\'s First Name', value: '{{user.firstName}}' },
+		{ label: 'User\'s Last Name', value: '{{user.lastName}}' },
+		{ label: 'User\'s Full Name', value: '{{user.fullName}}' }
+	],
 	// This value must be kept in sync with the language defined in webpack.config.js.
 	language: 'en'
 };
